4b01c12ead625509bdcb340fd581a48345414f42
Sayi <adasai90@gmail.com>
2019-02-27 20:46:08 +0800

    init repo

---
+++ cargo-infrastructure/src/main/java/com/deepoove/cargo/infrastructure/App.java
@@ -0,0 +1,13 @@
+package com.deepoove.cargo.infrastructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cargo-web/src/main/java/com/deepoove/cargo/web/CargoApplication.java
@@ -0,0 +1,14 @@
+package com.deepoove.cargo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableCaching
+public class CargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargoApplication.class, args);
+    }
+
+}
